4fcdb5a076219cbabe092d8ef4c5c28424fca87d
fmoreno <fmoreno@starbucks.com>
2024-07-21 00:56:37 -0400

    add secrets and add build

--- mypizza/mypizza/settings.py
+++ mypizza/mypizza/settings.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
 """
 Django settings for mypizza project.
 
@@ -10,7 +14,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-'
-'6qe5rq1pj6v3_*e$c3*v-qt9_t9!c3*u!a69=(thyk(sx=%wco'
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY', '')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
